41f6b8b3750c97e3c259eabeff48499bc82f6ff6
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-02-25 21:24:22 +0530

    Implement create and get tasks with pagination, filtering, and search

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,8 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const generateToken = require("../utils/generateToken");
+
 
 exports.register = async (req, res) => {
     try {
@@ -24,6 +27,45 @@ exports.register = async (req, res) => {
 
         res.status(201).json({
             message: "User registered successfully"
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        const token = generateToken(user._id);
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: false, // change to true in production
+            sameSite: "strict"
+        });
+
+        res.status(200).json({
+            message: "Login successful"
         });
 
     } catch (error) {
